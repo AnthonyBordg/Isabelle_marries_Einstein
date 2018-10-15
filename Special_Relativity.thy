698a79e00e97ebeff4c95094e581d2fe06a4d52d
Anthony Bordg <bordg.anthony@gmail.com>
2018-10-15 16:45:34 +0100

    Update Special_Relativity.thy

--- Special_Relativity.thy
+++ Special_Relativity.thy
@@ -22,7 +22,7 @@ We define the interval between any two events
 definition interval :: "[real^4, real^4] \<Rightarrow> real" where
 "interval u w \<equiv> -(w$3 - u$3)\<^sup>2 + (w$0 - u$0)\<^sup>2 + (w$1 - u$1)\<^sup>2 + (w$2 - u$2)\<^sup>2"
 
-text \<open>We define a boost of velocity v in the u$0 direction\<close>
+text \<open>We define a boost of velocity v in the u$0 direction.\<close>
 
 definition boost_0 :: "[real^4, real] \<Rightarrow> real^4" where
 "boost_0 u v \<equiv> 
@@ -34,7 +34,7 @@ u$2,
 (u$3/sqrt(1 - v\<^sup>2)) - (v * u$0)/sqrt(1 - v\<^sup>2)
 ]
 "
-text \<open>We define a boost of velocity v in the u$1 direction\<close>
+text \<open>We define a boost of velocity v in the u$1 direction.\<close>
 
 definition boost_1 :: "[real^4, real] \<Rightarrow> real^4" where
 "boost_1 u v \<equiv> 
@@ -46,7 +46,7 @@ u$2,
 (u$3/sqrt(1 - v\<^sup>2)) - (v * u$1)/sqrt(1 - v\<^sup>2)
 ]
 "
-text \<open>We define a boost of velocity v in the u$2 direction\<close>
+text \<open>We define a boost of velocity v in the u$2 direction.\<close>
 
 definition boost_2 :: "[real^4, real] \<Rightarrow> real^4" where
 "boost_2 u v \<equiv> 
@@ -58,6 +58,23 @@ u$2/sqrt(1 - v\<^sup>2) - (v * u$3)/sqrt(1 - v\<^sup>2),
 (u$3/sqrt(1 - v\<^sup>2)) - (v * u$2)/sqrt(1 - v\<^sup>2)
 ]
 "
+
+text \<open>We prove the invariance of the interval with respect to a boost.\<close>
+
+lemma invar_0:
+  fixes v:: "real" and u w::"real^4"
+  assumes "v\<^sup>2 < 1"
+  shows "interval u w = interval (boost_0 u v) (boost_0 w v)"
+proof-
+  fix v::"real" and u w::"real^4"
+  have "interval (boost_0 u v) (boost_0 w v) = 
+-((w$3/sqrt(1 - v\<^sup>2) - (v * w$0)/sqrt(1 - v\<^sup>2)) - u$3/sqrt(1 - v\<^sup>2) + (v * u$0)/sqrt(1 - v\<^sup>2))\<^sup>2 + 
+((w$0/sqrt(1 - v\<^sup>2) - (v * w$3)/sqrt(1 - v\<^sup>2)) - u$0/sqrt(1 - v\<^sup>2) + (v * u$3)/sqrt(1 - v\<^sup>2))\<^sup>2 + 
+(w$1 - u$1)\<^sup>2 + 
+(w$2 - u$2)\<^sup>2"
+    apply (auto intro: field_simps simp add: interval_def boost_0_def)
+    
+
 
 
 
