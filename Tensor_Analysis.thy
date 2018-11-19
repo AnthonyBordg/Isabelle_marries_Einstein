7cf642b9fd6f1bdf3dce6a2e7ae2fbee98237aea
Anthony Bordg <bordg.anthony@gmail.com>
2018-11-19 17:28:49 +0000

    Update Tensor_Analysis.thy

--- Tensor_Analysis.thy
+++ Tensor_Analysis.thy
@@ -33,6 +33,27 @@ in order to distinguish it from the corresponding vector space of vectors.
 definition one_form_to_fun :: "one_form \<Rightarrow> real^4 \<Rightarrow> real" where
 "one_form_to_fun f \<equiv> Rep_one_form f"
 
+lemma one_form_eq_prelim:
+  fixes f::"one_form" and g::"one_form"
+  assumes "one_form_to_fun f = one_form_to_fun g"
+  shows "f = g"
+  using assms one_form_to_fun_def
+  by (simp add: Rep_one_form_inject)
+
+lemma one_form_to_fun_eq:
+  fixes f::"one_form" and g::"one_form"
+  assumes "\<forall>x::real^4. one_form_to_fun f x = one_form_to_fun g x"
+  shows "one_form_to_fun f = one_form_to_fun g"
+  using assms
+  by (simp add: ext)
+
+lemma one_form_eq:
+  fixes f::"one_form" and g::"one_form"
+  assumes "\<forall>x::real^4. one_form_to_fun f x = one_form_to_fun g x"
+  shows "f = g"
+  using assms one_form_eq_prelim one_form_to_fun_eq 
+  by simp
+
 text\<open>We introduce a coercion from one_forms to functions.\<close>
 
 declare [[coercion one_form_to_fun]]
@@ -121,20 +142,113 @@ qed
 definition scaleR_one_form :: "real \<Rightarrow> one_form \<Rightarrow> one_form" where
 "scaleR_one_form r f \<equiv> Abs_one_form (\<lambda>v. r * f(v))"
 
+(*declare [[show_sorts,show_types,show_consts]]*)
+
 instance
 proof 
-  show "\<And>u v w::one_form. u + v + w = u + (v + w)"
+  show "\<And>f g h::one_form. f + g + h = f + (g + h)"
   proof-
-    fix u v w::"one_form"
-    have "(u + v + w) x = (u + (v + w)) x" for x::"real^4"
+    fix f g h::"one_form"
+    have "(f + g + h) x = (f + (g + h)) x" for x::"real^4"
       using plus_one_form_def Rep_one_form one_form_to_fun_def semiring_normalization_rules(25)
       by (metis (mono_tags, lifting) Abs_one_form_inverse islinear_plus_one_form mem_Collect_eq)
-    thus "u + v + w = u + (v + w)"
-      by (metis Rep_one_form_inverse ext one_form_to_fun_def)
+    thus "f + g + h = f + (g + h)"
+      using one_form_eq 
+      by simp
   qed
-
-        
-
+  show "\<And>f g::one_form. f + g = g + f"
+  proof-
+    fix f g::"one_form"
+    have "(f + g) x = (g + f) x" for x::"real^4"
+      using plus_one_form_def Rep_one_form
+      by (simp add: linordered_field_class.sign_simps(27))
+    thus "f + g = g + f"
+      using one_form_eq 
+      by simp
+  qed
+  show "\<And>f::one_form. 0 + f = f"
+  proof-
+    fix f::"one_form"
+    have "(0 + f) x = f x" for x::"real^4"
+      using plus_one_form_def Rep_one_form
+      by (simp add: zero_one_form_def Abs_one_form_inverse module_hom_zero one_form_to_fun_def)
+    thus "0 + f = f"
+      using one_form_eq 
+      by simp
+  qed
+  show "\<And>f::one_form. - f + f = (0::one_form)"
+  proof-
+    fix f::"one_form"
+    have "(- f + f) x = (0::one_form) x" for x::"real^4"
+      using plus_one_form_def uminus_one_form_def Rep_one_form zero_one_form_def Abs_one_form_inverse 
+        islinear_uminus_one_form one_form_to_fun_def 
+      by simp
+    thus "- f + f = (0::one_form)"
+      using one_form_eq 
+      by simp
+  qed
+  show "\<And>(f::one_form) g::one_form. f - g = f + - g"
+  proof-
+    fix f::"one_form" and g::"one_form"
+    have "(f - g) x = (f + - g) x" for x::"real^4"
+      using minus_one_form_def uminus_one_form_def plus_one_form_def Abs_one_form_inverse 
+        islinear_uminus_one_form one_form_to_fun_def 
+      by simp
+    thus "f - g = f + - g"
+      using one_form_eq 
+      by simp
+  qed
+  show "\<And>(r::real) f g::one_form. r *\<^sub>R (f + g) = r *\<^sub>R f + r *\<^sub>R g"
+  proof-
+    fix r::real and f g::one_form
+    have "(r *\<^sub>R (f + g)) x = (r *\<^sub>R f + r *\<^sub>R g) x" for x::"real^4"
+      using plus_one_form_def scaleR_one_form_def ring_class.ring_distribs(1)
+      by (metis (mono_tags, lifting) Abs_one_form_inverse islinear_plus_one_form 
+          islinear_scaleR_one_form mem_Collect_eq one_form_to_fun_def)
+    thus "r *\<^sub>R (f + g) = r *\<^sub>R f + r *\<^sub>R g"
+      using one_form_eq 
+      by simp
+  qed
+  show "\<And>r (s::real) f::one_form. (r + s) *\<^sub>R f = r *\<^sub>R f + s *\<^sub>R f"
+  proof-
+    fix r s::real and f::one_form
+    have f1:"((r + s) *\<^sub>R f) x = (r + s) * f(x)" for x::"real^4"
+      using scaleR_one_form_def Abs_one_form_inverse islinear_scaleR_one_form one_form_to_fun_def 
+      by simp
+    have f2:"(r *\<^sub>R f + s *\<^sub>R f) x = r * f(x) + s * f(x)" for x::"real^4"
+      using scaleR_one_form_def plus_one_form_def
+      by (metis (no_types, lifting) Abs_one_form_inverse islinear_plus_one_form islinear_scaleR_one_form 
+          mem_Collect_eq one_form_to_fun_def)
+    from f1 and f2 have "((r + s) *\<^sub>R f) x = (r *\<^sub>R f + s *\<^sub>R f) x" for x::"real^4"
+      using scaleR_one_form_def plus_one_form_def
+      by (simp add: distrib_right)
+    thus "(r + s) *\<^sub>R f = r *\<^sub>R f + s *\<^sub>R f"
+      using one_form_eq 
+      by simp
+  qed
+  show "\<And>r (s::real) f::one_form. r *\<^sub>R s *\<^sub>R f = (r * s) *\<^sub>R f"
+  proof-
+    fix r s::real and f::one_form
+    have "(r *\<^sub>R s *\<^sub>R f) x = ((r * s) *\<^sub>R f) x" for x::"real^4"
+      using scaleR_one_form_def
+      by (metis Abs_one_form_inverse islinear_scaleR_one_form linordered_field_class.sign_simps(23) 
+          mem_Collect_eq one_form_to_fun_def)
+    thus "r *\<^sub>R s *\<^sub>R f = (r * s) *\<^sub>R f"
+      using one_form_eq 
+      by simp
+  qed
+  show "\<And>f::one_form. 1 *\<^sub>R f = f"
+  proof-
+    fix f::one_form
+    have "(1 *\<^sub>R f) x = f x" for x::"real^4"
+      using scaleR_one_form_def
+      by (simp add: Rep_one_form_inverse one_form_to_fun_def)
+    thus "1 *\<^sub>R f = f"
+      using one_form_eq 
+      by simp
+  qed
+qed
+      
 end
 
 
